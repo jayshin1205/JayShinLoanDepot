0e282295257b7a04c1c6ffaf300db86442f42d50
jayshin1205 <64346596+jayshin1205@users.noreply.github.com>
2020-04-26 17:15:49 -0700

    project.ps1

---
+++ project.ps1
@@ -0,0 +1,64 @@
+<#
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+#>
+
+# find all the multiples of 3 and  5 under 1000 and add them up
+function test {
+    param (
+
+        [Parameter(Mandatory=$true)][int] $FirstNum,
+        [Parameter(Mandatory=$true)][int] $SecondNum,
+        [Parameter()][switch] $NoDuplicates
+    )
+
+    # initialize variables
+    $mOfFirst = 0
+    $mOfSecond = 0
+    $lessThanValue = 1000
+    $firstSum = @()
+    $secondSum = @()
+    $total = @()
+    $final = 0
+    [int] $i = 1
+
+    # find all the multiples of 1st number and 2nd number
+    # multiples of 1st number or multiples of 2nd number is less than 1000
+    while (($mOfFirst -lt $lessThanValue) -or ($mOfSecond -lt $lessThanValue))
+    {    
+        # multiples
+        $mOfFirst = $FirstNum * $i
+        $mOfSecond = $SecondNum * $i
+
+        # multiples of 1st num is less than 1000
+        if($mOfFirst -lt $lessThanValue)
+        {
+            $firstSum += $mOfFirst
+        }
+
+        # multiples of 2nd num is less than 1000
+        if ($mOfSecond -lt $lessThanValue)
+        {
+            $secondSum += $mOfSecond
+        }
+
+        $i++
+    }
+
+    if ($NoDuplicates) # no duplicates
+    {
+        $total = $firstSum + $secondSum | select -Unique
+    }
+    else
+    {
+        $total = $firstSum + $secondSum
+    }
+
+    foreach ($num in $total)
+    {
+        $final += $num
+    }
+    return $final
+}
